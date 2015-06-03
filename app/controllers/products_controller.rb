737e3ee3fcda7e4e6b9043606b86b6e110d2cecb
iamsanchez <sedsanchez93@gmail.com>
2015-06-03 11:26:16 -0600

    pasdasdadsas pruev
    intent

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     # @products = Product.order("Category_id ASC")
-   @ja = Product.find_by_sql('SELECT products.id, products."Name", products."Description",products."Price", categories.id as CID, categories."Name" as CategoryName FROM products 
+   @ja = Product.find_by_sql('SELECT * FROM products 
   INNER JOIN categories ON products."Category_id" = categories.id
   ORDER BY products."Category_id", products."Price"')
 
